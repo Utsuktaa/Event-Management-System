f5cbc71ced39eb7ddb18c7593d0faf28bd49ae94
utsukta <utsuktashukla@gmail.com>
2026-05-25 14:23:14 +0545

    fix react routing on vercel

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,14 +1,14 @@
-const {
-  register,
-  login,
-  googleAuth,
-} = require("../controllers/auth.controller");
+  const {
+    register,
+    login,
+    googleAuth,
+  } = require("../controllers/auth.controller");
 
-const router = require("express").Router();
+  const router = require("express").Router();
 
-router
-  .post("/register", register)
-  .post("/login", login)
-  .post("/google", googleAuth);
+  router
+    .post("/register", register)
+    .post("/login", login)
+    .post("/google", googleAuth);
 
-module.exports = router;
+  module.exports = router;
